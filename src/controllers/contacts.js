7375bee259d8d2a79741bde2bd741b0528bbbf17
olgafedorec <olga_fedorec@yahoo.com>
2024-09-08 22:56:57 +0300

    create crud

---
+++ src/controllers/contacts.js
@@ -0,0 +1,64 @@
+import createHttpError from "http-errors";
+import * as contactServices from "../services/contacts.js";
+
+export const getAllContactsController = async (req, res) => {
+    
+        const data = await contactServices.getAllContacts();
+    
+        res.json({
+          status: 200,
+          message: 'Successfully found contacts!',
+          data,
+        });
+  };
+
+  export const getContactByIdController = async(req, res) => {
+        const { id } = req.params;
+        const data = await contactServices.getContactById(id);
+    
+        if(!data) {
+          throw createHttpError(404, 'Contact not found');
+        }
+    
+        res.json({
+          status: 200,
+          message: `Successfully found contact with id ${id}!`,
+          data,
+        });
+};
+
+export const addContactController = async(req,res) => {
+      const data = await contactServices.createContact(req.body);
+      res.status(201).json({
+        status: 201,
+        message: "Successfully created a contact!",
+        data,
+      });
+};
+
+
+export const patchContactController = async(req, res) => {
+    const { id } = req.params;
+    const result = await contactServices.updateContact({_id: id}, req.body);
+
+    if(!result) {
+      throw createHttpError(404, 'Contact not found');
+    }
+
+    res.json({
+      status: 200,
+      message: "Successfully patched a contact!",
+      data: result.data,
+    });
+};
+
+export const deleteContactController = async(req, res) => {
+    const { id } = req.params;
+    const data = await contactServices.deleteContact({_id: id});
+
+    if(!data) {
+      throw createHttpError(404, 'Contact not found');
+    }
+
+   res.status(204).send();
+};
--- src/server.js
+++ src/server.js
@@ -1,60 +1,23 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
 import { env } from './utils/env.js';
-import * as contactServices from "./services/contacts.js";
+import notFoundHandler from './middlewares/notFoundHandler.js';
+import errorHandler from './middlewares/errorHandler.js';
+import logger from './middlewares/logger.js';
+import contactsRouter from './routers/contacts.js';
 
 export const startServer = () => {
   const app = express();
-
-  const logger = pino({
-    transport: {
-        target: "pino-pretty"
-    }
-  });
 
   app.use(logger);
   app.use(cors());
   app.use(express.json());
 
-  app.get("/contacts", async (req, res) => {
-    const data = await contactServices.getAllContacts();
-    
-    res.json({
-      status: 200,
-      message: "Successfully found contacts!",
-      data,
-    });
-  });
+  app.use("/contacts", contactsRouter);
 
-  app.get("/contacts/:id", async(req, res) => {
-      const { id } = req.params;
-      const data = await contactServices.getContactById(id);
+  app.use(notFoundHandler);
 
-      if(!data) {
-        return res.status(404).json({
-          message: "Contact not found"
-        });
-      }
-
-      res.json({
-        status: 200,
-        message: `Successfully found contact with id ${id}!`,
-        data,
-      });
-  });
-
-  app.use((req, res) => {
-    res.status(404).json({
-        message: `${req.url} not found`
-    });
-  });
-
-  app.use((error, req, res, next) => {
-    res.status(500).json({
-        message: error.message,
-    });
-  });
+  app.use(errorHandler);
 
   const port = Number(env("PORT", 3000));
 
